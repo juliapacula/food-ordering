6c394c6d19cebb2a3ad9c6a6bfca3be6a0ecf48b
Julia Pacuła <paculajulia@gmail.com>
2020-09-09 22:10:34 +0200

    Added migration for order expanded columns

--- Migrator/Migrations/20200909200956_RenamedOrderColumns.Designer.cs
+++ Migrator/Migrations/20200909200956_RenamedOrderColumns.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DatabaseStructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Migrator.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200909200956_RenamedOrderColumns")]
+    partial class RenamedOrderColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrator/Migrations/20200909200956_RenamedOrderColumns.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Migrator.Migrations
+{
+    public partial class RenamedOrderColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FlatNumber",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetNumber",
+                table: "Orders",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FlatNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "StreetNumber",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
